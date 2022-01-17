adce20e4143ca72acf59761bfbcc8fd4417710b0
준범 구 <9junbeum@naver.com>
2022-01-17 16:52:30 +0900

    1. 폴더 열기 -> 폴더 선택 2. 폴더에 있는 사진 목록 표시 3. 목록 더블클릭 후 사진 표시

--- practice_2_이미지뷰어/Form1.Designer.cs
+++ practice_2_이미지뷰어/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "폴더 열기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(443, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 463);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.Location = new System.Drawing.Point(8, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(35, 14);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "주소 :";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 400);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(952, 482);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "K 이미지 뷰어";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- practice_2_이미지뷰어/Form1.cs
+++ practice_2_이미지뷰어/Form1.cs
@@ -12,9 +12,59 @@ namespace practice_2_이미지뷰어
 {
     public partial class Form1 : Form
     {
+        // 불러올 폴더 브라우저 띄워서 정보 담는 변수
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //폴더 브라우저로 부터 정보를 받아 가지고오면,
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                //선택된 주소를 textbox1에 표시
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+                //파일 목록 표시하는 곳 깨끗이 리셋하고
+                listBox1.Items.Clear();
+                //새로 받아온 목록 표시
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(folderBrowserDialog.SelectedPath);
+                System.IO.FileInfo[] fi = di.GetFiles();
+                
+                foreach(System.IO.FileInfo f in fi)
+                {
+                    if(f.Extension == ".jpg" || f.Extension == ".jpeg" || f.Extension == ".gif" || f.Extension == ".png" || f.Extension == ".jfif")
+                    {
+                        listBox1.Items.Add(f);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //인덱스 저장할 변수
+            int selectedIndex = -1;
+
+            //마우스 포인터 위치
+            Point point = e.Location;
+
+            selectedIndex = listBox1.IndexFromPoint(point);
+
+            if(selectedIndex != -1)
+            {
+                string selectedItem = listBox1.Items[selectedIndex].ToString();
+                string whatisfile = folderBrowserDialog.SelectedPath.ToString() + "\\" + selectedItem.ToString();
+                pictureBox1.Image = Image.FromFile(whatisfile);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
         }
     }
 }
